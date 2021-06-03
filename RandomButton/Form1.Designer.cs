2159a6a4ed9b63b732f7c8b3c3839ecde97e9424
Tural <tural.spotify.com@gmail.com>
2021-06-03 21:58:33 +0400

    Add project files.

---
+++ RandomButton/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace RandomButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.runBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // runBtn
+            // 
+            this.runBtn.AutoSize = true;
+            this.runBtn.BackColor = System.Drawing.Color.Chartreuse;
+            this.runBtn.Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runBtn.Location = new System.Drawing.Point(363, 218);
+            this.runBtn.Name = "runBtn";
+            this.runBtn.Size = new System.Drawing.Size(163, 41);
+            this.runBtn.TabIndex = 0;
+            this.runBtn.Text = "CATCH ME";
+            this.runBtn.UseVisualStyleBackColor = false;
+            this.runBtn.MouseHover += new System.EventHandler(this.runBtn_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.runBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runBtn;
+    }
+}
+
---
+++ RandomButton/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void runBtn_MouseHover(object sender, EventArgs e)
+        {
+            if (sender is Button bt)
+            {
+                bt.Dispose();
+                Button button = new Button()
+                {
+                    AutoSize = true,
+                    BackColor = System.Drawing.Color.Chartreuse,
+                    Font = new System.Drawing.Font("Algerian", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0))),
+                    Location = new System.Drawing.Point(363, 218),
+                    Name = "runBtn",
+                    Size = new System.Drawing.Size(163, 41),
+                    TabIndex = 0,
+                    Text = "Catch Me",
+                    UseVisualStyleBackColor = true
+                };
+                button.Size = new Size(140, 50);
+                Random random = new Random();
+                int randomX = random.Next(this.Size.Width - 100);
+                int randomY = random.Next(this.Size.Height - 100);
+                button.BackColor = Color.Chartreuse;
+                button.Location = new Point(randomX, randomY);
+                button.MouseHover += runBtn_MouseHover;
+                this.Controls.Add(button);
+            }
+        }
+    }
+}
